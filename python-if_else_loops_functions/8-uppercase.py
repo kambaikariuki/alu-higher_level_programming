d30e2edacc1d0a973dad8aa775761f2b98becd59
Kambai Kariuki <kariukikambai@gmail.com>
2024-06-27 14:48:05 +0300

    .

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for ord(i) in str
+    for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-        i = chr(ord('i') - 32)
-        print("{:s}".format(i), end="")
+            i = chr(ord('i') - 32)
+            print("{:s}".format(i), end="")
     print()
