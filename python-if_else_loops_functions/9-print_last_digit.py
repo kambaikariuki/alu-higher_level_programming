e2916731fea434a4ff1c07bdfad00154fd342210
Kambai Kariuki <kariukikambai@gmail.com>
2024-06-27 15:30:01 +0300

    .

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    count = 0
+    if number < 0:
+        count = 1
+        if count = 1:
+            number *= -1
+            last_digit = number % 10
+            print("{:d}".format(last_digit))
+            
+        elif:
+         number == 0:
+         print("{:d}".format(number))
+    else:
+       last_digit = number % 10
+       print("{:d}".format(last_digit))    
