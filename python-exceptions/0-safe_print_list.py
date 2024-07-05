0cb38cdc732672fce4e7ea2d44206027bfd2e6d0
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-05 08:47:17 +0300

    .

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    index =  0
+    index = 0
     while True:
         try:
-            if index < 0:                
-                print(my_list[index], end="")
+            if index < x:
+                print(my_list[index], end='')
                 index += 1
             else:
                 print()
-                return index    
+                return index
         except IndexError:
             print()
             return index
-    
