bdcb383e31ea4ed10de1b80f1b51400c6bc0bc20
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-09 13:16:53 +0300

    .

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result: {:d}".format(result))
+        print("Inside result: {:.1f}".format(result))
     except:
         print("None")
+        print("Inside result: {}".format(result))
     finally:
         return result
- 
