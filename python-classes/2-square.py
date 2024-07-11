32f9aacc66800da72952074e3cc510c4a15418e9
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-11 11:55:03 +0300

    .

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Class square defined by instantiation with optional size
+'''
+
+class Square:
+    '''
+Class square defined by instantiation with optional size
+'''
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
