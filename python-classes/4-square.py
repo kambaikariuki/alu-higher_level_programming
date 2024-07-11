ae72c5de6861c5fc9939de4dd5b7ca3e093c5587
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-11 12:19:17 +0300

    .

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,30 +6,24 @@ Class square defined by instantiation with optional size
 
 class Square:
     '''
-    Class square defined by instantiation with optional size
+    Initialize class square
     '''
+    @property
     def __init__(self, size=0):
-        if type(size) != int:
-                raise TypeError("size must be an integer")
-        if size < 0:
-                raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
     '''
     Returns area of square 
     '''
     def area(self):
-        return (self.__size * self.__size)
-    '''
-    Getting size
-    '''
+        return (self.size * self.size)
+    
     def size(self):
-          return self.__size
-    '''
-    Setting new size
-    '''
+          return self.size
+    
+    @size.setter
     def size(self, value):
         if type(value) != int:
                 raise TypeError("size must be an integer")
         if value < 0:
                 raise ValueError("size must be >= 0")
-        self.__size = value
+        self.size = value
