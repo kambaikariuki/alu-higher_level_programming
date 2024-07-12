0b046749e4a5bcf8b004cb0d3a55e75520ecfb79
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-12 11:51:54 +0300

    .

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-'''
-Square that defines a square by: (based on 3-square.py)
-
-Private instance attribute: size:
-property def size(self): to retrieve it
-property setter def size(self, value): to set it
+'''Initialize Class Square
 '''
 
 
 class Square:
+    '''Initialize Class Square
     '''
-    Initialize class square
-    '''
-    @property
+
     def __init__(self, size=0):
         self.size = size
-    '''
-    Returns area of square 
-    '''
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
         return (self.size * self.size)
-    
-    def size(self):
-          return self.size
-    
-    @size.setter
-    def size(self, value):
-        if type(value) != int:
-                raise TypeError("size must be an integer")
-        if value < 0:
-                raise ValueError("size must be >= 0")
-        self.size = value
