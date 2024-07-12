0554ee00c466813639dd0217985de29586c56e12
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-12 12:51:37 +0300

    .

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,34 +5,33 @@ Class rectangle
 
 
 class Rectangle:
+    '''Initialize class width
     '''
-    Initialize width
-    '''
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
 
-    '''Retrieving width '''
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
     @property
     def width(self):
         return self.width
     
-    '''Setting width'''
+    @property
+    def height(self):
+        return self.height
+    
     @width.setter
     def width(self, value):
         if type(value) != int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
+        self.__width = value
     
-    '''Retrieving height'''
-    def height(self):
-        return self.height
-    
-    '''Setting height'''
     @height.setter
     def height(self, value):
         if type(value) != int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
+        self.__height = value
