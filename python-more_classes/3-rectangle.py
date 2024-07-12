086be04c0fc4e466180632119fd958d19a3fac31
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-12 13:23:33 +0300

    .

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Defining a rectangle
+"""
+
+
+class Rectangle:
+    """Initialising"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((self.__height + self.__width) * 2)
+
+    def __str__(self):
+        return "\n".join("#" * self.__width for _ in range(self.__height))
