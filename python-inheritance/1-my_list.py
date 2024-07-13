b182f4f5946820a83d9cd5df3cf841880420a96a
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-13 16:17:56 +0300

    .

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+"""
+Defines an inherited list class MyList
+"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
-
+    """Inherits from list
+    """
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
+        """Prints the list in ascending order
+        """
         print(sorted(self))
