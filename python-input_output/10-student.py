dd1715d3dd44456c4bd00be40dc0598ffb7c4fc4
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-19 12:03:47 +0300

    .

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-'''
- a class Student that defines a student
-'''
+"""10-student module
+"""
 
 
 class Student:
-    '''module class student
-    '''
-
+    """Student class
+    """
     def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
+        """Instantiation
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        '''method to_json
-        '''
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of Student instance
+        """
+        if attrs is not None:
+            my_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
         return self.__dict__
