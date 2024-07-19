29012798d9da74ab7c3838b7e1a825de92c1dd47
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-19 11:46:35 +0300

    .

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,10 @@ returns the number of characters added
 """
 
 def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file
+    returns the number of characters added
+    """
+
     with open(filename, 'a') as f:
         return f.write(text)
