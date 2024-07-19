d8be52f644a12d5f3e4489fbfa58ff2891644bb5
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-19 12:08:04 +0300

    .

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-''' 
+""" 
 adds all arguments to a Python list,then saves them to a file
-'''
+"""
 
 
 import json
 import sys
-
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
