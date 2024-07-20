34f23d3bb010da3cb0812b233d005811e927009b
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-20 11:13:03 +0300

    .

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,6 +3,6 @@
 -- Password should be user_0d_1_pwd 
 -- If user already exists, script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
