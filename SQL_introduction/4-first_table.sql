9336739261febc3ca497df44d29d77cb9417e954
Kambai Kariuki <kariukikambai@gmail.com>
2024-07-20 10:29:03 +0300

    .

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table in the current database
+-- 	id INT
+--	name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256));
