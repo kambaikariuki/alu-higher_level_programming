6cfca14745a9f554d9aee17c65707fbd06d8d3f1
Kambai Kariuki <kariukikambai@gmail.com>
2024-08-02 10:40:41 +0200

    .

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a GET request to URL and displays body of response
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# sends GET request to given URL, with key-value, and displays body of response
+curl -sH "X-HolbertonSchool-User-Id:98" "$1"
