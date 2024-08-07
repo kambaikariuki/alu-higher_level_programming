db3e017e7c89c78310b92398beeb4019d69ea63b
Kambai Kariuki <kariukikambai@gmail.com>
2024-08-07 13:34:46 +0200

    .

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Doc"""
 
+
 def text_indentation(text):
     """Doc"""
-    
+
     if type(text) is not str:
         raise TypeError ("text must be a string")
-   
+
     previous = ""
     for char in text:
         # leading whitespace
