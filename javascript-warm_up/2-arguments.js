27517a29bcae92436349041032d6410d19e9a702
Kambai <kariukikambai@gmail.com>
2024-09-27 12:15:39 +0200

    .

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);// Exclude the first two elements (node and script path)
+const args = process.argv.slice(2);
 const argCount = args.length;
 
-if(argCount === 0){
-    console.log('No argument');
-}else if(argCount === 1){
-    console.log('Argument found');
-}else{
-    console.log('Arguments found');
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
