e27b5466c77e30fddec7f5856e769b7bb2bb150f
Kambai <kariukikambai@gmail.com>
2024-09-29 16:58:36 +0200

    .

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,7 @@
+#!/usr/bin/node
+// Script class Rectangle that defines a rectangle
 
-module.exports = class Rectangle {
+const Rectangle = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -8,8 +10,15 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+    let prints = '';
+    for (let cont1 = 0; cont1 < this.height; cont1++) {
+      for (let cont = 0; cont < this.width; cont++) {
+        prints = prints + 'X';
+      }
+      console.log(prints);
+      prints = '';
     }
   }
 };
+
+module.exports = Rectangle;
