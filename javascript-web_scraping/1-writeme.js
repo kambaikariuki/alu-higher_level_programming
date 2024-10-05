3334465891ca6ed6fdad7dbe2c6ff85d7ec54f55
Kambai <kariukikambai@gmail.com>
2024-10-05 23:20:52 +0200

    .

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs')
-const file = process.argv[2]
-const string = process.argv[3]
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
 
 fs.appendFile(`${file}`, `${string}`, function (error) {
-    if (error)
-        console.log(error);
-})
+  if (error) { console.log(error); }
+});
